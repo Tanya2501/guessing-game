acb7cf6e4f23662796df02bf6b20801f895e4d4b
Tanya2501 <tanya.bahmatovich@tut.by>
2017-01-29 23:29:19 +0200

    BSA/Feature: myImplementation

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      var result;
+      var number;
+    }
 
     setRange(min, max) {
-
+      var min=min;
+      var max=max;
+var average=guess (min,max);
+while(average!==number){
+if (number<average){
+lower(min, average);
+      }
+      else if (number>average){
+        greater(average, max);
+      }
+    }
     }
 
     guess() {
-
+      return (max-min)/2;
     }
 
     lower() {
-
+setRange(min, average);
     }
 
     greater() {
-
+setRange(-min, average);
     }
 }
 
